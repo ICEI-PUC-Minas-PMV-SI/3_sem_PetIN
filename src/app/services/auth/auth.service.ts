98fc386720792f349725a91e94d3e29d577c3974
Michael <michaelhenriqued@gmail.com>
2023-12-17 21:28:58 -0300

    feat(auth): add authentication logic

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable, from } from 'rxjs';
 import {
   LoginHTTPRequestBody,
@@ -9,28 +10,49 @@ import {
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  isLogged = false;
+  constructor() {
+    const storage = localStorage.getItem('user');
+    const userLogged = storage ? JSON.parse(storage) : undefined;
+    if (userLogged?.email && userLogged.password) {
+      this.isLogged = true;
+    }
+    console.log(this.isLogged);
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    return new Promise((resolve) => this.isLogged);
+  }
 
   login(body: LoginHTTPRequestBody): Observable<LoginHTTPResponse> {
-    const users = JSON.parse(localStorage.getItem('users') ?? '') as {
+    const users = JSON.parse(localStorage.getItem('users') ?? '[]') as {
       email: string;
       password: string;
     }[];
-    const user = (users ?? []).find((user) => {
+    const user = users.find((user) => {
       return user.email === body.email && user.password === body.password;
     });
     if (user) {
       localStorage.setItem('user', JSON.stringify(body));
+      this.isLogged = true;
       return from(
         new Promise<LoginHTTPResponse>((resolve, reject) =>
           resolve({ status: 200, data: { id: 'uuid', email: body.email } })
         )
       );
     }
+    this.isLogged = false;
     return from(
       new Promise<LoginHTTPResponse>((resolve, reject) =>
         reject({ status: 401, error: 'Credenciais inválidas' })
       )
     );
+  }
+  logout() {
+    localStorage.setItem('user', '');
+    this.isLogged = false;
   }
 }
